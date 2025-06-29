3540fd6be7baf6471418e7e1b982043e84ea8094
Valeska Valentin Ekklesia <106671548+ValeskaLim@users.noreply.github.com>
2025-06-30 00:44:13 +0700

    add 404 pages for undefined routes

---
+++ src/pages/Layout/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+function NotFoundPage() {
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+        <h1 className="text-4xl font-bold text-center text-gray-200">404<br/>Page Not Found</h1>
+        </div>
+    );
+}
+
+export default NotFoundPage;
