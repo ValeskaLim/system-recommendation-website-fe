07a231d40ae2dda3d59bd1f8093872e558fcded5
ValeskaLim <valeskavalentinekklesia@gmail.com>
2025-07-03 20:55:29 +0700

    add react icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,17 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { FaHome } from "react-icons/fa";
 
 const Sidebar = () => {
     return (
-        <aside className="w-64 bg-gray-800 text-white p-4">
-            <h2 className="text-xl font-bold mb-4">My App</h2>
+        <aside className="w-64 bg-gray-800 text-white py-4">
+            <h2 className="pl-4 text-xl font-bold mb-4">My App</h2>
             <nav>
-                <ul className="space-y-2">
-                    <li><Link to="/home" className="hover:underline">Home</Link></li>
-                    <li><Link to="/profile" className="hover:underline">Profile</Link></li>
+                <ul>
+                    <li>
+                        <NavLink to="/home" className={({ isActive }) => 
+                            `flex w-full py-2 pl-4 duration-300 hover:bg-blue-400 hover:duration-300 ${isActive ? 'bg-blue-400' : ''}`}><FaHome className='text-2xl mr-2'/>Home</NavLink>
+                    </li>
+                    <li><NavLink to="/profile" className="block w-full py-2 pl-4 duration-300 hover:bg-blue-400 hover:duration-300">Profile</NavLink></li>
                 </ul>
             </nav>
         </aside>
