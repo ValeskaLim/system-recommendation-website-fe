394067fcfe8f130141c3d81a0aecfe459ea4d343
ValeskaLim <valeskavalentinekklesia@gmail.com>
2025-08-09 18:45:55 +0700

    reformat certain pages

--- src/pages/Competition/AddCompetitionPage.tsx
+++ src/pages/Competition/AddCompetitionPage.tsx
@@ -1,5 +1,4 @@
 import { ROUTE_PATHS } from "../../router/routePaths";
-import { MdOutlineArrowBackIosNew } from "react-icons/md";
 import { useToast } from "../../hooks/useToast";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -7,155 +6,176 @@ import CommonConstant from "../../constant/CommonConstant";
 import { useState } from "react";
 
 const COMPETITION_TYPES = [
-    { label: 'Artificial Intelligence', value: 'AI' },
-    { label: 'Software Development', value: 'SD' },
-    { label: 'Mobile Development', value: 'MD' },
+  { label: "Artificial Intelligence", value: "AI" },
+  { label: "Software Development", value: "SD" },
+  { label: "Mobile Development", value: "MD" },
 ];
 
 const AddCompetitionPage = () => {
-    const [title, setTitle] = useState("");
-    const [date, setDate] = useState("");
-    const [status, setStatus] = useState("");
-    const [type, setType] = useState("");
-    const [slot, setSlot] = useState<number | undefined>(undefined);
-    const [description, setDescription] = useState("");
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [status, setStatus] = useState("");
+  const [type, setType] = useState("");
+  const [slot, setSlot] = useState<number | undefined>(undefined);
+  const [description, setDescription] = useState("");
 
-    const { errorToast, successToast } = useToast();
+  const { errorToast, successToast } = useToast();
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            if (slot === undefined || slot < 0) {
-                errorToast("Slot must be a non-negative number");
-                return;
-            }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (slot === undefined || slot < 0) {
+        errorToast("Slot must be a non-negative number");
+        return;
+      }
 
-            const existing_data = await axios.post(CommonConstant.GetExistingCompetition, {
-                title, date
-            });
-
-            if (existing_data.data.isExist === true) {
-                errorToast(`Competition ${title} is already exist!`);
-                return;
-            }
-
-            await axios.post(CommonConstant.AddCompetition, {
-                title, date, status, type, slot, description
-            });
-            navigate(ROUTE_PATHS.COMPETITION);
-            successToast("Competition successfully added");
-        } catch (error) {
-            console.log(error);
-            errorToast("Error adding competition");
+      const existing_data = await axios.post(
+        CommonConstant.GetExistingCompetition,
+        {
+          title,
+          date,
         }
-    };
+      );
 
-    return (
-        <div className="flex flex-col">
-            <div className="">
-                <div className="flex w-fit mb-5">
-                    <button onClick={() => navigate(-1)}><MdOutlineArrowBackIosNew className="cursor-pointer flex items-center text-2xl mr-2 font-semibold" /></button>
-                    <h1 className="text-3xl">Add Competition Data</h1>
-                </div>
-                <form method="POST" onSubmit={handleSubmit} className="space-y-4">
-                    <div className="flex justify-between">
-                        <label className="text-lg items-center flex w-64">Title</label>
-                        <input
-                            type="text"
-                            id="title"
-                            placeholder="Input competition title"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                            className="p-1 w-full border border-gray-500 rounded text-gray-900 placeholder:text-gray-400 focus:outline"
-                            required
-                        />
-                    </div>
-                    <div className="flex justify-between">
-                        <label className="text-lg items-center flex w-64">Date</label>
-                        <input
-                            type="date"
-                            id="date"
-                            placeholder="Input competition title"
-                            value={date}
-                            onChange={(e) => setDate(e.target.value)}
-                            className="p-1 w-full border border-gray-500 rounded text-gray-900 placeholder:text-gray-400 focus:outline"
-                            required
-                        />
-                    </div>
-                    <div className="flex justify-between">
-                        <label className="text-lg items-center flex w-64">Status</label>
-                        <select
-                            name="status"
-                            id="status"
-                            value={status}
-                            onChange={(e) => setStatus(e.target.value)}
-                            className="p-1 w-full border border-gray-500 rounded text-gray-900 placeholder:text-gray-400 focus:outline"
-                            required
-                        >
-                            <option value="" hidden>
-                                --Select one--
-                            </option>
-                            <option value="ACT">Active</option>
-                            <option value="INA">Inactive</option>
-                        </select>
-                    </div>
-                    <div className="flex justify-between">
-                        <label className="text-lg items-center flex w-64">Type</label>
-                        <select
-                            name="type"
-                            id="type"
-                            value={type}
-                            onChange={(e) => setType(e.target.value)}
-                            className="p-1 w-full border border-gray-500 rounded text-gray-900 placeholder:text-gray-400 focus:outline"
-                            required
-                        >
-                            <option value="" hidden>
-                                --Select one--
-                            </option>
-                            {COMPETITION_TYPES.map((t) => (
-                                <option key={t.value} value={t.value}>{t.label}</option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className="flex justify-between">
-                        <label className="text-lg items-center flex w-64">Slot</label>
-                        <input
-                            type="number"
-                            id="slot"
-                            min="0"
-                            placeholder="Input initial slot"
-                            value={slot ?? ""}
-                            onChange={(e) => {
-                                const value = parseInt(e.target.value, 10);
-                                setSlot(isNaN(value) ? undefined : value);
-                            }}
-                            required
-                            className="p-1 w-full border border-gray-500 rounded text-gray-900 placeholder:text-gray-400 focus:outline"
-                        />
-                    </div>
-                    <div className="flex justify-between">
-                        <label className="text-lg items-center flex w-64">Description</label>
-                        <textarea
-                            name="description"
-                            id="description"
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                            className="p-1 w-full border border-gray-500 rounded text-gray-900 placeholder:text-gray-400 focus:outline"
-                            required>
-                        </textarea>
-                    </div>
-                    <button
-                        type="submit"
-                        className="w-fit bg-blue-500 text-white mt-5 py-2 px-4 rounded-md duration-300 font-semibold hover:bg-blue-600 hover:duration-300"
-                    >
-                        Submit
-                    </button>
-                </form>
-            </div>
+      if (existing_data.data.isExist === true) {
+        errorToast(`Competition ${title} is already exist!`);
+        return;
+      }
+
+      await axios.post(CommonConstant.AddCompetition, {
+        title,
+        date,
+        status,
+        type,
+        slot,
+        description,
+      });
+      navigate(ROUTE_PATHS.COMPETITION);
+      successToast("Competition successfully added");
+    } catch (error) {
+      console.log(error);
+      errorToast("Error adding competition");
+    }
+  };
+
+  return (
+    <div className="flex flex-col">
+      <div className="">
+        <div className="flex w-fit mb-5">
+          <h1 className="text-3xl">Add Competition Data</h1>
         </div>
-    )
-}
+        <form method="POST" onSubmit={handleSubmit} className="space-y-4">
+          <div className="flex justify-between">
+            <label className="text-lg items-center flex w-64">Title</label>
+            <input
+              type="text"
+              id="title"
+              placeholder="Input competition title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="p-1 w-full border border-gray-500 rounded text-gray-900 placeholder:text-gray-400 focus:outline"
+              required
+            />
+          </div>
+          <div className="flex justify-between">
+            <label className="text-lg items-center flex w-64">Date</label>
+            <input
+              type="date"
+              id="date"
+              placeholder="Input competition title"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              className="p-1 w-full border border-gray-500 rounded text-gray-900 placeholder:text-gray-400 focus:outline"
+              required
+            />
+          </div>
+          <div className="flex justify-between">
+            <label className="text-lg items-center flex w-64">Status</label>
+            <select
+              name="status"
+              id="status"
+              value={status}
+              onChange={(e) => setStatus(e.target.value)}
+              className="p-1 w-full border border-gray-500 rounded text-gray-900 placeholder:text-gray-400 focus:outline"
+              required
+            >
+              <option value="" hidden>
+                --Select one--
+              </option>
+              <option value="ACT">Active</option>
+              <option value="INA">Inactive</option>
+            </select>
+          </div>
+          <div className="flex justify-between">
+            <label className="text-lg items-center flex w-64">Type</label>
+            <select
+              name="type"
+              id="type"
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+              className="p-1 w-full border border-gray-500 rounded text-gray-900 placeholder:text-gray-400 focus:outline"
+              required
+            >
+              <option value="" hidden>
+                --Select one--
+              </option>
+              {COMPETITION_TYPES.map((t) => (
+                <option key={t.value} value={t.value}>
+                  {t.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="flex justify-between">
+            <label className="text-lg items-center flex w-64">Slot</label>
+            <input
+              type="number"
+              id="slot"
+              min="0"
+              placeholder="Input initial slot"
+              value={slot ?? ""}
+              onChange={(e) => {
+                const value = parseInt(e.target.value, 10);
+                setSlot(isNaN(value) ? undefined : value);
+              }}
+              required
+              className="p-1 w-full border border-gray-500 rounded text-gray-900 placeholder:text-gray-400 focus:outline"
+            />
+          </div>
+          <div className="flex justify-between">
+            <label className="text-lg items-center flex w-64">
+              Description
+            </label>
+            <textarea
+              name="description"
+              id="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              className="p-1 w-full border border-gray-500 rounded text-gray-900 placeholder:text-gray-400 focus:outline"
+              required
+            ></textarea>
+          </div>
+          <div className="flex gap-3">
+            <button
+              type="submit"
+              className="cursor-pointer w-fit bg-blue-500 text-white mt-5 py-2 px-4 rounded-md duration-300 font-semibold hover:bg-blue-600 hover:duration-300"
+            >
+              Submit
+            </button>
+            <button
+              type="button"
+              onClick={() => navigate(-1)}
+              className="cursor-pointer w-fit text-blue-500 border-2 mt-5 py-2 px-4 rounded-md duration-300 font-semibold hover:text-blue-600 hover:duration-300"
+            >
+              Back
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default AddCompetitionPage;
