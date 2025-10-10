850635d0c451d318bc1c1567436ef8d63a2bc572
ValeskaLim <valeskavalentinekklesia@gmail.com>
2025-10-11 03:55:43 +0700

    Finishing homepage

---
+++ src/components/SectionCard.tsx
@@ -0,0 +1,88 @@
+import BlueButton from "./BlueButton";
+
+interface SectionCardProps {
+  title: string;
+  link: string;
+  description: string;
+  imageSrc: string;
+  imageAlign?: "left" | "right";
+  customSizeImage?: string;
+  className?: string;
+  extendedClassName?: string;
+  customColor?: string;
+}
+
+const colorMap: Record<string, string> = {
+  green: "border-green-200 bg-green-100",
+  blue: "border-blue-200 bg-blue-100",
+  red: "border-red-200 bg-red-100",
+  yellow: "border-yellow-200 bg-yellow-100",
+  amber: "border-amber-200 bg-amber-100",
+  orange: "border-orange-200 bg-orange-100",
+  pink: "border-pink-200 bg-pink-100",
+};
+
+const SectionCard = ({
+  title,
+  link,
+  description,
+  imageSrc,
+  imageAlign = "left",
+  customSizeImage = "w-[200px] h-[170px]",
+  className,
+  extendedClassName = "",
+  customColor,
+}: SectionCardProps) => {
+  return (
+    <div
+      className={`${
+        className ?? `border-2 p-6 rounded-lg w-fit ${extendedClassName}`
+      } ${colorMap[customColor] ?? "border-black"}`}
+    >
+        {imageAlign === "left" ? (
+            <div className="flex justify-between gap-20">
+                <div>
+                <div className="flex items-center gap-3 mb-3">
+                    <h2 className="text-[2em] font-bold">{title}</h2>
+                </div>
+                <p className="my-5 text-lg">{description}</p>
+                <BlueButton
+                    label="Learn more"
+                    onClick={() => (window.location.href = link)}
+                    extendedClassName="mt-3"
+                />
+                </div>
+                <div>
+                <img
+                    src={imageSrc}
+                    alt="Card Thumbnail"
+                    className={customSizeImage}
+                />
+                </div>
+            </div>
+        ) : (
+            <div className="flex justify-between gap-20">
+                <div>
+                    <img
+                        src={imageSrc}
+                        alt="Card Thumbnail"
+                        className={customSizeImage}
+                    />
+                </div>
+                <div>
+                    <div className="flex items-center gap-3 mb-3">
+                        <h2 className="text-[2em] font-bold">{title}</h2>
+                    </div>
+                    <p className="my-5 text-lg">{description}</p>
+                    <BlueButton
+                        label="Learn more"
+                        onClick={() => (window.location.href = link)}
+                        extendedClassName="mt-3"
+                    />
+                </div>
+            </div>
+        )}
+    </div>
+  );
+};
+export default SectionCard;
--- src/pages/Layout/HomePage.tsx
+++ src/pages/Layout/HomePage.tsx
@@ -1,19 +1,66 @@
+import SectionCard from "../../components/SectionCard";
 import WelcomeMessage from "../../components/WelcomeMessage";
 import { useAuth } from "../../hooks/AuthProvider";
+import { ROUTE_PATHS } from "../../router/routePaths";
 
 const HomePage = () => {
   const { users } = useAuth();
 
   return (
-    <div className="main-container">
-      <div className="main-col-container">
-        <img src="logo.png" alt="Logo" className="w-[200px] h-[200px]"/>
-        <h1 className="text-5xl font-bold my-4">Welcome to Sunib HALL</h1>
+    <div className="main-container mx-40">
+      <div className="main-col-container items-center">
+        <img src="logo.png" alt="Logo" className="w-[200px] h-[200px]" />
+        <h1 className="text-5xl font-bold my-8">Welcome to Sunib HALL</h1>
         <WelcomeMessage user={users?.fullname} />
-        <p className="text-lg text-center mt-2">
-          Your all-in-one platform for organizing teammates and managing
-          competition teams
+        <p className="text-3xl text-center mt-15 italic">
+          "Your all-in-one platform to find and join competitions with ease!"
         </p>
+        <div className="w-full flex flex-col gap-10 mt-40">
+          <div className="w-full flex justify-start">
+            <SectionCard
+              title="Find & Join Competition Now"
+              link={ROUTE_PATHS.COMPETITION}
+              description="Find and join competitions that suit you best."
+              imageSrc="undoukai_trophy.png"
+              customColor="red"
+              customSizeImage="w-[250px] h-[200px]"
+            />
+          </div>
+          <div className="w-full flex justify-end">
+            <SectionCard
+              title="Find Team Members"
+              link={ROUTE_PATHS.FIND}
+              description="Find member criteria that suit you best and invite them to join your team."
+              imageSrc="friends_man.png"
+              extendedClassName="justify-items-end"
+              imageAlign="right"
+              customSizeImage="w-[300px] h-[200px]"
+              customColor="orange"
+            />
+          </div>
+          <div className="w-full flex justify-start">
+            <SectionCard
+              title="Teammates List"
+              link={ROUTE_PATHS.TEAMMATES_LIST}
+              description="View your team and teammates details."
+              imageSrc="online_kaigi_man.png"
+              customSizeImage="w-[300px] h-[300]"
+              customColor="green"
+            />
+          </div>
+          <div className="w-full flex justify-end">
+            <SectionCard
+              title="Your Profile"
+              link={ROUTE_PATHS.PROFILE}
+              description="View and edit your profile information."
+              imageSrc="mirror_woman_smile.png"
+              extendedClassName="justify-items-end"
+              imageAlign="right"
+              customSizeImage="w-[250px] h-[250px]"
+              customColor="pink"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
